d8388fd1d287e725b6fe67da56c72d541bad9606
Account-Personal <494671601a@gmail.com>
2022-03-12 18:05:52 +0100

    remove unused variable

--- idea.sol
+++ idea.sol
@@ -81,7 +81,6 @@ contract Magazzino {
     //la funzione che mi consente di vedere i lotti di un determinato materia prima
     function vediLottiMateriaPrima(uint256 _nomeMateriaPrima /*id della materia prima */)  public view returns (string[] memory){
         string[] memory result = new string[](numMateriePrime);      
-        uint j = 0;
         MateriaPrima memory materia = nomiMateriePrime[_nomeMateriaPrima];
         for(uint i=0; i<=materia.lottoMaterie.length; i++){
             result[i] = string(abi.encodePacked(materia.nomeMateriaPrima,materia.lottoMaterie[i]));
