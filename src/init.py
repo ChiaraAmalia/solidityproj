ad49a8b2be950fc9559a28b12af5af5bdcb92d82
MargheritaGaleazzi <64259274+MargheritaGaleazzi@users.noreply.github.com>
2022-04-06 07:52:41 +0200

    corretto

--- src/init.py
+++ src/init.py
@@ -39,30 +39,24 @@ w3 = Web3(web3.HTTPProvider("http://127.0.0.1:22000"))
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 w3.middleware_onion.add(web3.middleware.http_retry_request_middleware)
 
-
-if len(w3.eth.accounts) == 1:
-    w3.geth.personal.new_account('trasformatore')
-    w3.geth.personal.new_account('produttore')
-    w3.geth.personal.new_account(('consumatore'))
-
 print(w3.geth.personal.list_accounts())
 
-
-print(Web3.toChecksumAddress(w3.eth.accounts[0])+" è il trasformatore")
+print(Web3.toChecksumAddress(w3.eth.accounts[0])+" è il consumatore")
 print(Web3.toChecksumAddress(w3.eth.accounts[1])+" è il produttore")
-print(Web3.toChecksumAddress(w3.eth.accounts[2])+" è il consumatore")
+print(Web3.toChecksumAddress(w3.eth.accounts[2])+" è il trasformatore")
 
 
 Consumatore = w3.eth.contract(abi=abi, bytecode=bytecode)
 
 # Invia la transazione che fa il deploy del contratto
-trasf=Web3.toChecksumAddress(w3.eth.accounts[0])
-w3.geth.personal.unlock_account(w3.eth.accounts[0], 'trasformatore')
+consum=Web3.toChecksumAddress(w3.eth.accounts[0])
+w3.geth.personal.unlock_account(w3.eth.accounts[0], 'consumatore')
 prod=Web3.toChecksumAddress(w3.eth.accounts[1])
 w3.geth.personal.unlock_account(w3.eth.accounts[1], 'produttore')
-consum=Web3.toChecksumAddress(w3.eth.accounts[2])
-w3.geth.personal.unlock_account(w3.eth.accounts[2], 'consumatore')
+trasf=Web3.toChecksumAddress(w3.eth.accounts[2])
+w3.geth.personal.unlock_account(w3.eth.accounts[2], 'trasformatore')
 admin=Web3.toChecksumAddress(w3.eth.accounts[3])
+w3.geth.personal.unlock_account(w3.eth.accounts[3], '')
 
 tx_hash = Consumatore.constructor(consum).transact({'from': admin})
 #Wait for the transaction to be mined, and get the transaction receipt
